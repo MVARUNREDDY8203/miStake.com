e50ba77866dfc827f554557f72d157361884cff6
M.Varun Reddy <varunmr2003@gmail.com>
2025-03-14 13:36:38 +0530

    advik reddy test commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="flex items-center justify-center bg-black text-white text-8xl">
+                advik reddy bohot badmaash hai
+            </div>
+        </>
+    );
+}
+
+export default App;
