4a525b76b6aad55a200e64215abe94a43505bc91
M.Varun Reddy <varunmr2003@gmail.com>
2025-03-14 13:43:38 +0530

    test#3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <>
-            <div className="h-screen w-screen flex items-center justify-center bg-black text-white text-8xl">
+            <div className="h-screen w-7xl flex items-center justify-center bg-black text-white text-8xl">
                 advik reddy bohot badmaash hai
             </div>
         </>
