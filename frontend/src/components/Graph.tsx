c7dc5d530bf4b4c16f0b4ad423637ba47d4caa18
M.Varun Reddy <varunmr2003@gmail.com>
2025-04-04 16:09:54 +0530

    live-stats/ graph feature for bets history added with a draggable modal type component #4

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,4 +1,4 @@
-import { AreaChart, Area, XAxis, YAxis, Tooltip } from "recharts";
+import { AreaChart, Area, XAxis, YAxis } from "recharts";
 import { useWalletStore } from "../stores/wallet";
 import { useEffect } from "react";
 import { DEFAULT_BALANCE } from "../constants/constants";
@@ -30,7 +30,18 @@ export default function Graph() {
     const dataMin: number = Math.min(...transactions.map((v, _) => v.balance));
     const dataMax: number = Math.max(...transactions.map((v, _) => v.balance));
     const yDomain = [dataMin - 1000, dataMax + 1000];
-
+    const customToolTip = ({
+        active,
+        payload,
+    }: {
+        active: any;
+        payload: any;
+    }) => {
+        if (active && payload && payload.length) {
+            return <div className="bg-gray-400">{payload[0].value}</div>;
+        }
+        return null;
+    };
     return (
         <div className="flex items-center justify-center rounded-xl bg-stake-700 w-[95%]">
             <AreaChart
@@ -47,7 +58,17 @@ export default function Graph() {
                 {/* <CartesianGrid strokeDasharray="3 3" /> */}
                 <XAxis dataKey="created_at" tick={false} axisLine={false} />
                 <YAxis domain={yDomain} tick={false} axisLine={false} />
-                <Tooltip />
+                {/* <Tooltip
+                    contentStyle={{
+                        backgroundColor: "#1a1a1a",
+                        borderRadius: "8px",
+                        border: "none",
+                        padding: "10px",
+                        color: "#ffffff",
+                    }}
+                    label={"balance"}
+                    active={false}
+                /> */}
                 <defs>
                     <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
                         <stop
